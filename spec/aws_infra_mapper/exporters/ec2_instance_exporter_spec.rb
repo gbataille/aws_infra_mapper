798778a25d6864828ecc7d2662295f8ca437b730
Grégory Bataille <gregory.bataille@gmail.com>
2018-04-21 14:51:28 +0200

    refactor(): remove unused test setup

--- spec/aws_infra_mapper/exporters/ec2_instance_exporter_spec.rb
+++ spec/aws_infra_mapper/exporters/ec2_instance_exporter_spec.rb
@@ -34,20 +34,4 @@ RSpec.describe AwsInfraMapper::Exporters::EC2InstanceExporter do
       end
     end
   end
-
-  describe '#as_edges' do
-    let(:src_instance) { build(:aws_ec2_instance) }
-    let(:user_id_group_pair) do
-      src_sg = src_instance.security_groups[0]
-      build(:aws_user_id_group_pair, group_id: src_sg.group_id, group_name: src_sg.group_name)
-    end
-    let(:ip_perm) { build(:aws_ip_permission, user_id_group_pairs: [user_group_id_pair]) }
-    let(:sg_to) { build(:aws_security_group, ip_permissions: [ip_perm]) }
-
-    let(:dest_instance_1) { build(:aws_ec2_instance, security_groups: [sg_to]) }
-    let(:dest_instance_2) { build(:aws_ec2_instance, security_groups: [sg_to]) }
-
-    it 'should link instances properly' do
-    end
-  end
 end
